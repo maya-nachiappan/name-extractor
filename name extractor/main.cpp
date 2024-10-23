1e32a15d43d7835c35859f7a537d34ee26c3655d
maya-nachiappan <maya.n102304@gmail.com>
2024-10-23 09:29:49 -0700

    extracts first and last name by checking for spaces

--- name extractor/main.cpp
+++ name extractor/main.cpp
@@ -4,11 +4,27 @@
 //
 //  Created by maya nachiappan on 10/23/24.
 //
-
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    std::string fullName;
+
+    std::cout << "Enter your full name (first and last): ";
+    std::getline(std::cin, fullName);
+
+    size_t spacePos = fullName.find(' ');
+
+    if (spacePos != std::string::npos) {
+        std::string firstName = fullName.substr(0, spacePos);
+        
+        std::string lastName = fullName.substr(spacePos + 1);
+
+        std::cout << "First Name: " << firstName << std::endl;
+        std::cout << "Last Name: " << lastName << std::endl;
+    } else {
+        std::cout << "No last name found! Please enter both first and last names." << std::endl;
+    }
+
     return 0;
 }
