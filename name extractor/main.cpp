9d184079d09740a610f02c05ea63c0610ec23a35
maya-nachiappan <maya.n102304@gmail.com>
2024-10-23 09:27:00 -0700

    Initial Commit

---
+++ name extractor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  name extractor
+//
+//  Created by maya nachiappan on 10/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
